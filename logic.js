9aeac3b102a3f46e2c5c60f152610dbb4c0a8816
TIhiGameing <167775368+TIhiGameing@users.noreply.github.com>
2026-02-06 00:12:41 +0100

    Refactor player ranking logic and add player class

--- logic.js
+++ logic.js
@@ -1,30 +1,66 @@
+
+window.onload = start;
+
+
 let json = fetch("./data.json")
 .then(response => response.json())
 .then(json => data_loaded(json));
 
-let player_rankings = {};
+
+let players = [
+    "Eivind", //0
+    "Jens", //1
+    "Ludvig", //2
+    "Adrian", //3
+    "Erik", //4
+    "Leah", //5
+    "Abdalla", //6
+    "Pete", //7
+    "Angus", //8
+    "Bendik" //9
+];
+
+let player_objects = [];
+let matchups = [];
+
+const K = 32;
 
 
-let base_player_value;
 
 
+//HTML
 let playerDiv
 let playerCheckboxes = [];
 
 
 
-window.onload = start;
-
 function start() {
-    // data_loaded();
+    
+    
 }
 
-function data_loaded(out) {
-    player_rankings = convert_winrate(out);
-    base_player_value = getPlayerValue(player_rankings);
+function data_loaded(data) {
+    for (let i = 0; i < players.length; i++) {
+        player_objects.push(new Player(i));
+    }
+
+    
+
+    for (let tourney of data) {
+        for (let match of tourney) {
+            let winner = match[0];
+            let loser = match[1];
+
+            CalculateRatingChange(winner, loser);            
+        }
+    }
+
+
+
+    //HTML
     playerDiv = document.getElementById("players");
 
-    for (const [key, value] of Object.entries(player_rankings)) {
+    for (const key of [...players].sort() ) {
         let player_label = document.createElement("label");
         player_label.htmlFor = key;
         player_label.textContent = key;
@@ -41,30 +77,32 @@ function data_loaded(out) {
         playerCheckboxes.push(player_checkbox);
     }
 
-    // player_rankings.forEach(player => {
-    //     
-    // });
-
     document.getElementById("toggle_button").onclick = toggle_on_click;
     document.getElementById("seed_button").onclick = seed_on_click;
 }
 
-function convert_winrate (data) {
-    let out = {};
-
-    for (const [player, opponents] of Object.entries(data)) {
-        out[player] = {};
-        for (const [opponent, winrate] of Object.entries(opponents)) {
-            let x = parseInt(winrate.split("/")[0])
-            let y = parseInt(winrate.split("/")[1])
-
-            out[player][opponent] = x/y * 100;
-        }
+class Player {
+    constructor(name) {
+        this.id = name;
+        this.name = players[name];
+        this.elo = 0;
     }
-
-    return out
 }
 
+function CalculateRatingChange(winner, loser)
+{
+    let Elo1 = player_objects[winner].elo;
+    let Elo2 = player_objects[loser].elo;
+    let EloDifference = Elo2 - Elo1;
+    let percentage = 1 / ( 1 + Math.pow( 10, EloDifference / 400 ) );
+    let win = Math.round( K * ( 1 - percentage ) );
+
+
+    player_objects[winner].elo += win;
+}
+
+
+//HTML
 function toggle_on_click() {
     let all_checked = true;
     playerCheckboxes.forEach(element => {
@@ -79,210 +117,31 @@ function toggle_on_click() {
 }
 
 function seed_on_click() {
-    let included_players = {};
+    let included_players = [];
 
     playerCheckboxes.forEach(element => {
-        element.checked ? included_players[element.name] = player_rankings[element.name] : null;
+        let player_id = players.indexOf(element.id);
+
+        element.checked ? included_players.push(player_objects[player_id]) : null;
     });
 
 
     let seedDiv = document.getElementById("seed");
     let scoreDiv = document.getElementById("score");
-    let valueDiv = document.getElementById("value");
 
-    
-    let seeding = getSeeding(included_players);
+    included_players = included_players.sort((a, b) => a.name.localeCompare(b.name));
+    let seeding = included_players.sort((a, b) => b.elo - a.elo);
+    console.log(seeding);
     
 
     let seedOutput = "Seeding order:<br>";
-    for (let i = 0; i < Object.keys(seeding).length; i++) {
-        seedOutput += (i + 1) + ". " + seeding[i]["player"] + "<br>";
-    }
-    seedDiv.innerHTML = seedOutput;
-
     let scoreOutput = "Player Scores:<br>";
-    let max_value = Math.max(...seeding.map(player => player.value));
-    for (let i = 0; i < Object.keys(seeding).length; i++) {
-        let score_str = (Math.round(seeding[i]["score"]/max_value * 100 * 10)/10).toString();
+    for (let i = 0; i < seeding.length; i++) {
+        seedOutput += (i + 1) + ". " + seeding[i].name + "<br>";
+
+        let score_str = seeding[i].elo.toString();
         scoreOutput += "Score " + score_str + "<br>";
     }
+    seedDiv.innerHTML = seedOutput;
     scoreDiv.innerHTML = scoreOutput;
-
-    let valueOutput = "Player Values:<br>";
-    for (let i = 0; i < Object.keys(seeding).length; i++) {
-        let value_str = (Math.round(seeding[i]["value"]/max_value * 100 * 10)/10).toString();
-        valueOutput += "Value " + value_str + "<br>";
-    }
-    valueDiv.innerHTML = valueOutput;
 }
-
-
-
-function getSeeding(players) {
-    let matchups = getMatchups(players);
-    let player_values = getPlayerValue(players);
-    
-    let scoring = {};
-    for (const player of Object.keys(players)) {
-        scoring[player] = [0];
-    }
-
-    for (const [matchup, winrate] of Object.entries(matchups)) {
-        let [player1, player2] = matchup.split(" vs ");
-        // if (winrate > 50) {
-        //     scoring[player1].push(player_values[player2]);
-        // } 
-        // else if (winrate < 50) {
-        //     scoring[player2].push(player_values[player1]);
-        // } 
-        // else {
-        //     scoring[player1].push(player_values[player2]/2);
-        //     scoring[player2].push(player_values[player1]/2);
-        // }
-
-        scoring[player1].push(player_values[player2] * (winrate / 100));
-        scoring[player2].push(player_values[player1] * ((100 - winrate) / 100));
-    }
-
-    if (Object.keys(matchups).length === 0) {
-        //Sorts based on average winrate
-        let seeding = {};
-        console.log(base_player_value)
-        for (const [player, rankings] of Object.entries(players)) {
-            let totaL = 0;
-            for (const [opponent, rank] of Object.entries(rankings)) {
-                totaL += base_player_value[opponent];
-            }
-
-            if (Object.keys(rankings).length === 0) {
-                seeding[player] = 0;
-            }
-            else {
-                seeding[player] = totaL / Object.keys(rankings).length;
-            }
-        }
-        const sortedSeeding = Object.keys(seeding)
-        .sort((a, b) => seeding[b] - seeding[a]);
-
-        
-        return getSeedOutput(sortedSeeding, seeding, base_player_value);
-    }
-
-    let seeding = {};
-    console.log(scoring);
-    for (const [player, score] of Object.entries(scoring)) {
-        //seeding[player] = (score.reduce((a, b) => a + b, 0) / score.length);
-        seeding[player] = Math.max(...score);
-
-        if (seeding[player] === 0 || score.length === 1) {
-            seeding[player] = averageWinrate(players, player)/100; 
-        }
-        //seeding[player] += player_values[player];   
-    }
-    const sortedSeeding = Object.keys(seeding)
-    .sort((a, b) => seeding[b] - seeding[a]);
-    
-    console.log(seeding);
-    //console.log(player_values);
-
-    
-    
-    return getSeedOutput(sortedSeeding, seeding, player_values);
-}
-
-function getSeedOutput(sortedSeeding, seeding, player_values) {
-    const outSeeding = [];
-    sortedSeeding.forEach((player) => {
-        outSeeding.push({
-            'player': player,
-            'score': Math.round(seeding[player]),
-            'value': Math.round(player_values[player])
-        })
-    })
-
-    return outSeeding
-}
-
-function getMatchups(players) {
-    let matchups = {};
-
-    for (const [player, opponents] of Object.entries(players)) {
-        for (const [opponent, winrate] of Object.entries(opponents)) {
-            let matchup_str = [player, opponent].sort().join(" vs ");
-
-            let plys = matchup_str.split(" vs ");
-            if (plys[0] in players && plys[1] in players) {
-                if (plys[0] === player) {
-                    matchups[matchup_str] = winrate;
-                }
-            }
-            
-            
-        }
-    }
-
-    return matchups;
-}
-
-function getPlayerValue(players) {
-    let player_values = {};
-
-    for (const player of Object.keys(players)) {
-        player_values[player] = 0;
-    }
-
-    for (let index = 0; index < 5 ; index++) {
-        for (const [player, rankings] of Object.entries(players)) {
-            if (player in players) {
-                let total = 0;
-                for (const [opponent, rank] of Object.entries(rankings)) {
-                    if (opponent in players) {
-                        if (index === 0) {
-                            total += rank;
-                        }
-                        else {
-                            total += (player_values[opponent]/100) * rank;
-                        }
-                    }
-                }
-
-                let length = Object.keys(rankings).length
-
-                if (!isNaN(total / length)) {
-                    //if (index === 0) {
-                        player_values[player] += total / length;
-                    // }
-                    // else {
-                    //     player_values[player] *= total / length;
-                    // }
-                    
-                    if (player === "Eivind") {
-                        //console.log(player_values[player]);
-                    }
-                }
-            }
-        }
-    }
-    
-
-    return player_values;
-}
-
-
-function averageWinrate(players, player) {
-    let player_data = players[player];
-
-    let total = 0;
-    for (const [opponent, rank] of Object.entries(player_data)) {
-        total += rank;
-    }
-
-    return total / Object.keys(player).length;
-
-}
-
-
-
-
-
-
